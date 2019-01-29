e11a9587db52c04e6a8aff77bad1e35cc0ed8348
FrierenZk <lingfengfroze@yahoo.com>
2019-01-29 19:42:57 +0800

    Modify log format

--- Log/FetchLog.go
+++ Log/FetchLog.go
@@ -12,9 +12,9 @@ var Log *log.Logger
 var logFile *os.File
 
 func LogInit() error {
-	log.SetFlags(log.LstdFlags|log.Lshortfile|log.Ltime|log.Llongfile)
-	logFile =openLog()
-	Log = log.New(logFile,"",log.LstdFlags|log.Lshortfile|log.Ltime|log.Llongfile)
+	log.SetFlags(log.LstdFlags | log.Lshortfile | log.Ltime)
+	logFile = openLog()
+	Log = log.New(logFile, "", log.LstdFlags|log.Lshortfile|log.Ltime)
 	Log.Println("---------------Log Start---------------")
 	return nil
 }
@@ -31,13 +31,13 @@ func LogExit(ec error) {
 	Log.Println("---------------Log End----------------")
 }
 
-func openLog() *os.File{
-	var filePath= envpath.GetAppDir()+"/logs/Fetch-YoC.log"
-	stat,err := os.Stat(filePath)
-	if err!=nil{
+func openLog() *os.File {
+	var filePath = envpath.GetAppDir() + "/logs/Fetch-YoC.log"
+	stat, err := os.Stat(filePath)
+	if err != nil {
 		log.Println(err)
-	}else {
-		log.Println("log size now is ",stat.Size())
+	} else {
+		log.Println("log size now is ", stat.Size())
 		if stat.Size() > 0x80000 {
 			err = os.Rename(filePath, filePath+"."+time.Now().Format("2006-1-2 15-04-05"))
 			if err != nil {
@@ -46,15 +46,19 @@ func openLog() *os.File{
 			}
 		}
 	}
-	dir,err := envpath.GetParentDir(filePath)
+	dir, err := envpath.GetParentDir(filePath)
 	err = envpath.CheckMakeDir(dir)
-	if err!=nil {
+	if err != nil {
 		log.Fatal(err)
 	}
-	file,err:= os.OpenFile(filePath, os.O_APPEND|os.O_CREATE, os.ModePerm)
+	file, err := os.OpenFile(filePath, os.O_APPEND|os.O_CREATE, os.ModePerm)
 	if err != nil {
 		log.Println("failed to load log file at: " + filePath)
 		log.Fatal(err)
+	}
+	_, err = file.WriteString("\n")
+	if err != nil {
+		log.Println(err)
 	}
 	return file
 }
