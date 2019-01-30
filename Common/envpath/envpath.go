c92a8ea43b80fb988e804b246ba8073a8da176f1
FrierenZk <lingfengfroze@yahoo.com>
2019-01-30 10:21:24 +0800

    Add func GetSubPath and GetSubFile

--- Common/envpath/envpath.go
+++ Common/envpath/envpath.go
@@ -1,6 +1,7 @@
 package envpath
 
 import (
+	"io/ioutil"
 	"log"
 	"os"
 	"path/filepath"
@@ -44,4 +45,31 @@ func CheckMakeDir(dir string) error {
 		err = os.Mkdir(dir, os.ModePerm)
 	}
 	return err
+}
+
+func GetSubPath(srcPath string,subDir string)(dstPath string,err error) {
+	if srcPath[len(srcPath)-1]=='/' {
+		srcPath = srcPath[:len(srcPath)-1]
+	}
+	if subDir[0]=='/' {
+		subDir = subDir[1:]
+	}
+	dstPath = srcPath + "/" + subDir
+	_, err = ioutil.ReadDir(dstPath)
+	if err != nil {
+		return "", err
+	}
+	return dstPath, nil
+}
+
+func GetSubFile(srcPath string,subFile string)(dstPath string,err error) {
+	if srcPath[len(srcPath)-1]=='/' {
+		srcPath = srcPath[:len(srcPath)-1]
+	}
+	dstPath = srcPath + "/" + subFile
+	_, err = ioutil.ReadFile(dstPath)
+	if err != nil {
+		return "", err
+	}
+	return dstPath, nil
 }
